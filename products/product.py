4a9030d9cddb809e386dc172e32a528ea635f710
Eran <e.magnex@gmail.com>
2018-05-14 01:57:01 +0300

    Generified product get and set TODO create table generification

--- products/product.py
+++ products/product.py
@@ -1,19 +1,54 @@
-from abc import ABCMeta, abstractmethod
-
-
 class Product(object):
-    __metaclass__ = ABCMeta
-
-    def __init__(self, name):
+    def __init__(self, category, name, parameters=[]):
         self.name = name
+        self.category = category
+        self.parameters = parameters
 
-    @abstractmethod
     def get_price(self, specification, db_handle):
-        pass
+        mattress_id = self._get_mattress_id(db_handle)
+        return db_handle.query_select(
+            'SELECT PRICE FROM {}_prices WHERE id = ? AND {}'.format(self.category, self._generate_params_condition()),
+            args=[mattress_id] + specification, only_first_result_needed=True)
 
-    @abstractmethod
     def set_price(self, specification, price, db_handle):
-        pass
+        mattress_id = self._get_mattress_id(db_handle)
 
+        if not mattress_id:
+            self._add_model_to_mattresses(db_handle)
+            mattress_id = self._get_mattress_id(db_handle)
 
+        current_price = self.get_price(specification, db_handle)
 
+        if not current_price:
+            set_price_result = db_handle.query_insert_or_update(
+                'INSERT INTO {}_prices (id, {}, price) VALUES (?,?,?,?)'.format(self.category,
+                                                                                self.generate_params_list()),
+                [mattress_id] + specification + [price])
+        else:
+            set_price_result = db_handle.query_insert_or_update(
+                'UPDATE {}_prices SET price=? WHERE id=? AND {}'.format(self.category, self._generate_params_condition()),
+                [price, mattress_id] + specification)
+
+        return set_price_result
+
+    def _add_model_to_mattresses(self, db_handle):
+        db_handle.query_insert_or_update(
+            'INSERT INTO {} (id, name, description) values(Null,?,Null)'.format(self.category),
+            [self.name])
+
+    def _get_mattress_id(self, db_handle):
+        return db_handle.query_select('SELECT id from {} where name=?'.format(self.category), args=[self.name],
+                                      only_first_result_needed=True)
+
+    def _generate_params_condition(self):
+        if len(self.parameters) > 0:
+            res = '{} = ?'.format(self.parameters[0])
+
+        for parameter in self.parameters[1:]:
+            res += ' AND {} = ?'.format(parameter)
+
+        return res
+
+    def _generate_params_list(self):
+        res = ', '.join(self.parameters)
+        return res
